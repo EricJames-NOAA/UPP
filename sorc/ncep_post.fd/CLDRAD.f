3fa3cb35b3da53774bb055fc41b32c4882df5bd0
Eric James <Eric.James@gaea66.ncrc.gov>
2026-02-18 12:49:43 -0500

    Getting rid of other CZMEAN checks in CLDRAD.f

--- sorc/ncep_post.fd/CLDRAD.f
+++ sorc/ncep_post.fd/CLDRAD.f
@@ -3813,11 +3813,7 @@ snow_check:   IF (QQS(I,J,L)>=QCLDmin) THEN
         DO J=JSTA,JEND
           DO I=ISTA,IEND
             IF(RSWOUT(I,J)<SPVAL) THEN
-             IF(CZMEAN(I,J)>1.E-6) THEN
                FACTRS=CZEN(I,J)/CZMEAN(I,J)
-             ELSE
-               FACTRS=0.0
-             ENDIF
              IF(RSWOUT(I,J)<SPVAL) GRID1(I,J)=RSWOUT(I,J)*FACTRS
             ENDIF
            ENDDO
@@ -3936,11 +3932,7 @@ snow_check:   IF (QQS(I,J,L)>=QCLDmin) THEN
          DO J=JSTA,JEND
            DO I=ISTA,IEND
             IF(RSWINC(I,J)<SPVAL) THEN
-             IF(CZMEAN(I,J)>1.E-6) THEN
                FACTRS=CZEN(I,J)/CZMEAN(I,J)
-             ELSE
-               FACTRS=0.0
-             ENDIF
              IF(RSWINC(I,J)<SPVAL) GRID1(I,J) = RSWINC(I,J)*FACTRS
             ENDIF
            ENDDO
